7fb82905bb55423ad7da162b4c6b48e294153e4e
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-06-18 10:47:56 -0700

    Create example messages and display in index.jade

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mini Message Board' });
+  res.render('index', { title: 'Mini Message Board', messages: messages });
 });
+
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 
 module.exports = router;
