a395cd104621df29d3b78196614f54dd88a74cf1
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-09-03 11:43:55 -0700

    Copy pool from Odin lesson

--- database/pool.js
+++ database/pool.js
@@ -1,4 +1,19 @@
-const { Pool } = require("pg");
 
 // Ok ---- 
 // Now we gotta access our sql database. 
+
+const { Pool } = require("pg");
+
+
+// Need to get user and password from environment variable. 
+// App is deployed.... So check deployment service for local environment variable. 
+
+
+
+// module.exports = new Pool({
+//   host: "localhost", // or wherever the db is hosted
+//   user: "<role_name>",
+//   database: "message_board",
+//   password: "<role_password>",
+//   port: 5432 // The default port
+// });
